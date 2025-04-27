c1901289fe414e993c525d2a421ecaf5f8fa5e50
Dhanvith Nayak <dhanvithnayak@proton.me>
2025-04-28 00:17:33 +0530

    add multi-os support?

--- lua/imvue/init.lua
+++ lua/imvue/init.lua
@@ -20,7 +20,18 @@ M.open_image = function(bufnr)
         return
     end
 
-    local cmd = 'xdg-open'
+    local cmd
+    if vim.fn.has('mac') == 1 then
+        cmd = 'open'
+    elseif vim.fn.has('unix') == 1 then
+        cmd = 'xdg-open'
+    elseif vim.fn.has('win32') == 1 then
+        cmd = 'start'
+    else
+        vim.notify('Unsupported OS for opening images', vim.log.levels.ERROR)
+        return
+    end
+
     vim.fn.jobstart({ cmd, full_path }, { detach = true })
 
     vim.schedule(function()
